ab9c84b9489af73c57447b74b158cae689c382f2
Oskar Kazmierczak <oskarkazmierczak1994@gmail.com>
2026-06-16 17:50:20 +0200

    Add model 'Unit' in models/models.py

---
+++ models/models.py
@@ -0,0 +1,23 @@
+from core.database import Base
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import String, SmallInteger
+
+class Unit(Base):
+    __tablename__ = "units"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    faction: Mapped[str] = mapped_column(String, nullable=False)
+    tier: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    hp: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    attack: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    defence: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    min_damage: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    max_damage: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    morale: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    luck: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    initiative: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    speed: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    upgrade: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    gold_cost: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    faction_resource_cost: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+
