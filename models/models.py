c6445918a089f12f7305445cbb91201b89b25b85
Oskar Kazmierczak <oskarkazmierczak1994@gmail.com>
2026-06-29 19:46:59 +0200

    Add creature type model

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 from core.database import Base
 from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import String, SmallInteger
+from sqlalchemy import String, SmallInteger, Boolean
 
 class Unit(Base):
     __tablename__ = "units"
@@ -21,3 +21,13 @@ class Unit(Base):
     gold_cost: Mapped[int] = mapped_column(SmallInteger, nullable=False)
     faction_resource_cost: Mapped[int] = mapped_column(SmallInteger, nullable=False)
 
+class CreatureType(Base):
+    __tablename__ = "creature_types"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+    min_morale: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    max_morale: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    min_luck: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    max_luck: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    necromancy: Mapped[bool] = mapped_column(Boolean, nullable=False)
