20c36c5f4bca84a9c72b0af2d9ddbddc5d61b91c
Oskar Kazmierczak <oskarkazmierczak1994@gmail.com>
2026-06-29 20:07:28 +0200

    Add populating creature types

--- scripts/manage_database.py
+++ scripts/manage_database.py
@@ -1,4 +1,4 @@
-from models.models import Unit
+from models.models import Unit, CreatureType
 from core.database import Base, engine, SessionLocal
 import argparse
 from pathlib import Path
@@ -7,14 +7,15 @@ from typing import Type, TypeVar
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("command", choices=["init", "drop", "seed"])
+parser.add_argument("command", choices=["init", "drop", "models","relationships"])
 args = parser.parse_args()
 
-units = Path("../seed/units")
+units = Path("seed/units")
+creature_types = Path("seed/abilities/passive_abilities")
 
 Model = TypeVar("Model", bound=Base)
 
-def seed_db(model: Type[Model], directory: Path, extension: str):
+def seed_models(model: Type[Model], directory: Path, extension: str):
     database = SessionLocal()
     try:
         for json_file in directory.glob(f"*.{extension}"):
@@ -30,10 +31,30 @@ def seed_db(model: Type[Model], directory: Path, extension: str):
     finally:
         database.close()
 
+def seed_relationship(model1, model2, model1_key, model2_key, model1_json_path: str, model2_json_path, association_json_path):
+    database = SessionLocal()
+    model1_json = json.load(open(model1_json_path))
+    model2_json = json.load(open(model2_json_path))
+    association_json = json.load(open(association_json_path))
+    for element in association_json:
+        model1_dict = model1_json.get(element.get(model1_key))
+        model2_dict = model2_json.get(element.get(model2_key))
+
+        unit = database.query(model1).filter_by(name=model1_dict.get("name")).first()
+
+        passive = database.query(model2).filter_by(name=model2_dict.get("name")).first()
+        unit.passive_abilities.append(passive)
+
+    database.commit()
 
 if args.command == "drop":
     Base.metadata.drop_all(bind=engine)
 if args.command == "init":
     Base.metadata.create_all(bind=engine)
-if args.command == "seed":
-    seed_db(Unit, units, "json")
+if args.command == "models":
+    seed_models(Unit, units, "json")
+    seed_models(CreatureType, creature_types, "json")
+'''if args.command == "relationships":
+    seed_relationship(Unit, PassiveAbility, "unit_id", "passive_id", 'seed/units/units_hive.json',
+                      'seed/abilities/passive_abilities/passive_abilities.json',
+                      "seed/associations/passive_unit_hive.json")'''
